92a14d035e858ec767db1cd9d63baccb6017ee62
Hasibullah Aman <hasib.aman7@gmail.com>
2023-04-08 12:06:50 +0430

    01_

--- Newslatter-Signup/app.js
+++ Newslatter-Signup/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
-
+// const bodyParser = require('body-parser');
+// const request = require('request');
 const app =  express();
 
 
 
+
 app.get('/', function(res,req){
-    res.send("Hello man I'm Here! ")
+    res.
 })
+
 
 
 
