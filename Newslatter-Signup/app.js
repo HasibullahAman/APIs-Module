65be60237daaba75ccb7712ac84de32fd157e0ae
Hasibullah Aman <hasib.aman7@gmail.com>
2023-04-19 21:31:46 +0430

    design done!

--- Newslatter-Signup/app.js
+++ Newslatter-Signup/app.js
@@ -1,22 +1,16 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const request = require('request');
-const app =  express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
 
 
 
 
-app.get('/', function(res,req){
+app.get('/', function(res, req) {
     req.send('Hi this is Hasibullah Aman');
 })
 
 
-
-
-
-
-
-
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log("Server runing on port 3000 Now:  ")
 })
